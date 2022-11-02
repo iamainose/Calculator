8433683a298037cf9ba21b0f3e0e53ad37305c4e
Davi_Ose <107152861+iamainose@users.noreply.github.com>
2022-11-02 12:42:46 -0700

    Completely changed JS code

--- script.js
+++ script.js
@@ -1,131 +1,164 @@
-let currentNum = "";
-let previousNum = "";
+let currentNumber = "";
+let prevNumber = "";
 let operator = "";
+let value = "";
+let result = "";
+let numbers = 0;
+let currentDisplay = document.querySelector(".currentNumber");
+let prevDisplay = document.querySelector(".previousNumber");
+let numberButtons = document.querySelectorAll(".number");
+let decimal = document.querySelector(".decimal");
+let equal = document.querySelector(".equal");
+let operators = document.querySelectorAll(".operator");
+let deletebtn = document.querySelector(".delete");
+let clear = document.querySelector(".clear");
+let sum = true;
 
-let currentDisplayNum = document.querySelector('.currentNumber');
-let previousDisplayNum = document.querySelector('.previousNumber');
-let numberButtons = document.querySelectorAll('.number');
-let decimal = document.querySelector('.decimal');
-let equal = document.querySelector('.equal');
-let operators = document.querySelectorAll('.operator');
-let deleteButton = document.querySelector('.delete');
-let output = document.querySelector('.output');
- equal = document.querySelector(".equal");
-equal.addEventListener("click", () => {
-  if (currentNum != "" && previousNum != "") {
-    calculator();
+window.addEventListener("keydown", keyBoardInput);
+
+numberButtons.forEach((number) => {
+  number.addEventListener("click", function (e) {
+    value = e.target.textContent;
+    appendNum(value);
+  });
+});
+
+operators.forEach((ops) => {
+  ops.addEventListener("click", (e) => {
+    value = e.target.textContent;
+    operate(value);
+  });
+});
+
+equal.addEventListener("click", (e) => {
+  if (prevNumber === "" && currentNumber !== "") {
+    currentDisplay.textContent = currentNumber;
+  } else {
+    this.sum = false;
+    value = e.target.textContent;
+    equate(value);
+    display(value);
   }
 });
 
-decimal = document.querySelector(".decimal");
-decimal.addEventListener("click", () => {
-  addDecimal();
-});
+decimal.addEventListener("click", addDecimals);
 
-const clear = document.querySelector(".clear");
-clear.addEventListener("click", clearCalculator);
+clear.addEventListener("click", clearScreen);
+deletebtn.addEventListener("click", del);
 
-deleteButton.addEventListener('click', function() {
-    currentNum = currentNum.slice(0, -1);
-    currentDisplayNum.textContent = currentNum;
-})
-
-
-numberButtons.forEach(num => {
-     num.addEventListener('click', (e) => {
-        handleNumber(e.target.textContent);
-        
-     })
-})
-
-
-function handleNumber(number) {
-    if (previousNum === "" && currentNum !== "" && operator === "") {
-      previousNum = "";
-      currentDisplayNum.textContent = currentNum;
-    }
-    if (currentNum.length <= 11) {
-      currentNum += number;
-      currentDisplayNum.textContent = currentNum;
-    }
+function calculate() {
+  currentNumber = Number(currentNumber);
+  prevNumber = Number(prevNumber);
+  if (operator === "+") {
+    prevNumber += currentNumber;
+  } else if (operator === "-") {
+    prevNumber -= currentNumber;
+  } else if (operator === "x") {
+    prevNumber *= currentNumber;
+  } else if (operator === "/") {
+    prevNumber /= currentNumber;
   }
-
-function calculator() {
-    previousNum = Number(previousNum);
-    currentNum = Number(currentNum);
-     if(operator === "+") {
-         previousNum += currentNum;
-     } else if(operator === "-") {
-         previousNum -= currentNum;
-     } else if(operator === "x") {
-         previousNum *= currentNum;
-     } else if(operator === "/") {
-        if (currentNum <= 0) {
-            previousNum = "Error";
-            displayResults();
-            return;
-        }
-        previousNum /= currentNum;
-     } 
-     previousNum = roundNumber(previousNum);
-     previousNum = previousNum.toString();
-     displayResults();
+  prevNumber = roundNumber(prevNumber);
+  prevNumber = prevNumber.toString();
+  display();
 }
 
-function roundNumber(num) {
-    return Math.round(num * 100000) / 100000;
+function operate(op) {
+  if (prevNumber === "") {
+    prevNumber = currentNumber;
+  } else if (operator === "/" && currentDisplay.textContent === "0") {
+    clearScreen();
+    alert("You can't divide by zero");
+  } else {
+    calculate();
+    operator = op;
+    currentDisplay.textContent = `${numbers}`;
+    prevDisplay.textContent = prevNumber + " " + operator;
+    currentNumber = result;
   }
-  
-  function displayResults() {
-    if (previousNum.length <= 11) {
-      currentDisplayNum.textContent = previousNum;
+}
+console.log(operate(operators));
+
+function equate() {
+  if (this.sum === false) {
+    if (operator === "/" && currentDisplay.textContent === "0") {
+      clearScreen();
+      alert("You can't divide by zero");
+    }
+    calculate();
+    prevDisplay.textContent = result;
+    currentDisplay.textContent = prevNumber;
+    currentNumber = result;
+    operator = result;
+  }
+}
+
+function roundNumber(number) {
+  return Math.round(number * 1000) / 1000;
+}
+
+function appendNum(number) {
+  if (currentNumber.length <= 11) {
+    currentNumber += number;
+    currentDisplay.textContent = currentNumber;
+  }
+}
+
+function clearScreen() {
+  operator = result;
+  currentNumber = result;
+  prevNumber = result;
+  prevDisplay.textContent = `${prevNumber}` + " " + `${operator}`;
+  currentDisplay.textContent = `${numbers}`;
+}
+
+function display() {
+  if (prevNumber.length <= 11) {
+    currentDisplay.textContent = prevNumber;
+  } else {
+    currentDisplay.textContent = prevNumber.slice(0, 11) + "...";
+  }
+  prevDisplay.textContent = `${result}`;
+  operator = `${result}`;
+  currentNumber = `${result}`;
+}
+
+function del() {
+  currentNumber = currentNumber.slice(0, -1);
+  currentDisplay.textContent = currentNumber;
+}
+
+function addDecimals() {
+  if (!currentNumber.includes(".")) {
+    currentNumber += ".";
+    currentDisplay.textContent = `${currentNumber}`;
+  }
+}
+
+function keyBoardInput(e) {
+  if (e.key >= 0 && e.key <= 9) {
+    appendNum(e.key);
+  } else if (e.key === ".") {
+    addDecimals();
+  } else if (e.key === "=" || e.key === "Enter") {
+    if (operator === "/" && currentDisplay.textContent === "0") {
+      alert("you can't divide by zero");
+      clearScreen();
     } else {
-      currentDisplayNum.textContent = previousNum.slice(0, 11) + "...";
+      calculate();
     }
-    previousDisplayNum.textContent = "";
-    operator = "";
-    currentNum = "";
+  } else if (e.key === "Escape") {
+    clearScreen();
+  } else if (e.key === "Backspace") {
+    del();
+  } else if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/") {
+    operate(convertInput(e.key));
   }
+}
 
-operators.forEach(op => {
-     op.addEventListener('click', function(e) {   
-        handleOperator(e.target.textContent);
-     })
-})
-
-function handleOperator(op) {
-    if (previousNum === "") {
-      previousNum = currentNum;
-      operatorCheck(op);
-    } else if (currentNum === "") {
-      operatorCheck(op);
-    } else {
-      calculator();
-      operator = op;
-      currentDisplayNum.textContent = "0";
-      previousDisplayNum.textContent = previousNum + " " + operator;
-    }
-  }
-  
-  function operatorCheck(text) {
-    operator = text;
-    previousDisplayNum.textContent = previousNum + " " + operator;
-    currentDisplayNum.textContent = "0";
-    currentNum = "";
-  }
-
-  function clearCalculator() {
-    currentNum = "";
-    previousNum = "";
-    operator = "";
-    currentDisplayNum.textContent = "0";
-    previousDisplayNum.textContent = "";
-  }
-  
-  function addDecimal() {
-    if (!currentNum.includes(".")) {
-      currentNum += ".";
-      currentDisplayNum.textContent = currentNum;
-    }
-  }
-
+function convertInput(keyboardOperator) {
+  if (keyboardOperator === "/") return "/";
+  if (keyboardOperator === "*") return "x";
+  if (keyboardOperator === "-") return "-";
+  if (keyboardOperator === "+") return "+";
+}
